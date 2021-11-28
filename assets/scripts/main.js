c101eddd02cab553ed21c0a401bb1ef02aa0dba8
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2021-11-28 20:30:33 +0100

    Add mobile navigation

---
+++ assets/scripts/main.js
@@ -0,0 +1,22 @@
+// This is NOT something we are familiar with by now, but needed to make it a bit more functional 
+
+// setup variables for things that are used more than once
+let headerElem = document.querySelector('header');
+let menuElem = document.querySelector('#menu');
+const strActive = 'opened';
+
+// Add class "opened" to <header> when #menu is clicked
+menuElem.addEventListener('click', function(event){
+	event.preventDefault(); // stops the element from doing its usual stuff
+
+  if (headerElem.className.indexOf('opened') === -1) {
+    // if the header element HAS the class 'opened'
+
+    headerElem.classList.add(strActive); // add the content of strActive to the class attribute on <header>
+    menuElem.innerHTML = 'CLOSE'; // change the text inside the button
+  } else {
+    // if the header element DOESN'T have the class 'opened'
+    headerElem.classList.remove(strActive); // add the content of strActive to the class attribute on <header>
+    menuElem.innerHTML = 'MENU'; // change the text inside the button
+  }
+});

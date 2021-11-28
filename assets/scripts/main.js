efe10ed283c36776672c38d888acfe7d07308293
Alexandru-Florin Bogdan <bogdan.alexandru1311@gmail.com>
2021-11-28 20:39:14 +0100

    Add responsive navigation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,12 +10,14 @@ menuElem.addEventListener('click', function(event){
 	event.preventDefault(); // stops the element from doing its usual stuff
 
   if (headerElem.className.indexOf('opened') === -1) {
-    // if the header element HAS the class 'opened'
+    // if the header element HAS the class 'opened' this code will be read
 
     headerElem.classList.add(strActive); // add the content of strActive to the class attribute on <header>
     menuElem.innerHTML = 'CLOSE'; // change the text inside the button
+    
   } else {
-    // if the header element DOESN'T have the class 'opened'
+    // if the header element DOESN'T have the class 'opened' this code will be read
+
     headerElem.classList.remove(strActive); // add the content of strActive to the class attribute on <header>
     menuElem.innerHTML = 'MENU'; // change the text inside the button
   }
